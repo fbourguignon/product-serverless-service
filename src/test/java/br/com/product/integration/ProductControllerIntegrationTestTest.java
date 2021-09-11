fb876c9df0fb20430e79078a70845bcfd55198be
Franklin Bourguignon <franklin_bourguignon@hotmail.com>
2021-09-11 19:49:05 -0300

    test:(refactoring tests)

--- src/test/java/br/com/product/integration/ProductControllerIntegrationTestTest.java
+++ src/test/java/br/com/product/integration/ProductControllerIntegrationTestTest.java
@@ -23,21 +23,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProductControllerIntegrationTestTest extends IntegrationControllerTest {
 
-    private String productId;
-    private final String PRODUCT_NAME = "Playstation 5";
-    private final Double PRODUCT_OLD_PRICE = 4500.00;
-    private final Double PRODUCT_NEW_PRICE = 4000.00;
-    private final String PRODUCT_DESCRIPTION = "New sony console";
-    private final String PRODUCT_CATEGORY = "Consoles";
-
     @Test
     @Order(1)
     void mustCreateProductWithSucess() throws JsonProcessingException {
-        final ProductRequest productRequest = ProductRequest.builder()
-                .name(PRODUCT_NAME)
-                .price(PRODUCT_OLD_PRICE)
-                .description(PRODUCT_DESCRIPTION)
-                .category(PRODUCT_CATEGORY)
+
+        var productRequest = ProductRequest.builder()
+                .name("Playstation 5")
+                .category("Console")
+                .description("Sony next generation console")
+                .price(4500.00)
                 .build();
 
         AwsProxyRequest request = new AwsProxyRequestBuilder("/products", HttpMethod.POST.toString())
@@ -48,25 +42,26 @@ public class ProductControllerIntegrationTestTest extends IntegrationControllerT
         final var response = handleApiGatewayRequest(request);
 
         final var productResponse = objectMapper.readValue(response.getBody(), ProductResponse.class);
-        this.productId = productResponse.getId();
 
         assertEquals(HttpStatus.CREATED.getCode(), response.getStatusCode());
         assertNotNull(productResponse.getId());
-        assertEquals(PRODUCT_NAME, productResponse.getName());
-        assertEquals(PRODUCT_CATEGORY, productResponse.getCategory());
-        assertEquals(PRODUCT_DESCRIPTION, productResponse.getDescription());
-        assertEquals(PRODUCT_OLD_PRICE, productResponse.getPrice());
+        assertEquals("Playstation 5", productResponse.getName());
+        assertEquals("Console", productResponse.getCategory());
+        assertEquals("Sony next generation console", productResponse.getDescription());
+        assertEquals(4500.00, productResponse.getPrice());
     }
 
     @Test
     @Order(2)
     void mustUpdateProductWithSucess() throws JsonProcessingException {
 
+        final var product = createProduct("Iphone 12", 6000.00, "Apple Smarthphone", "Smarthphone");
+
         final var productRequest = ProductRequest.builder()
-                .price(PRODUCT_NEW_PRICE)
+                .price(7500.00)
                 .build();
 
-        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(this.productId), HttpMethod.PUT.toString())
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(product.getId()), HttpMethod.PUT.toString())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .body(createJsonBoby(productRequest))
                 .build();
@@ -76,15 +71,16 @@ public class ProductControllerIntegrationTestTest extends IntegrationControllerT
         final var productResponse = objectMapper.readValue(apiGatewayResponse.getBody(), ProductResponse.class);
 
         assertEquals(HttpStatus.OK.getCode(), apiGatewayResponse.getStatusCode());
-        assertEquals(PRODUCT_NEW_PRICE, productResponse.getPrice());
+        assertEquals(7500.00, productResponse.getPrice());
     }
 
 
     @Test
     @Order(3)
     void mustGetProductWithSucess() throws JsonProcessingException {
+        final var product = createProduct("Xbox Series S", 2700.00, "Microsoft next generation console", "Console");
 
-        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(this.productId), HttpMethod.GET.toString())
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(product.getId()), HttpMethod.GET.toString())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .build();
 
@@ -93,39 +89,48 @@ public class ProductControllerIntegrationTestTest extends IntegrationControllerT
         final var productResponse = objectMapper.readValue(apiGatewayResponse.getBody(), ProductResponse.class);
 
         assertEquals(HttpStatus.OK.getCode(), apiGatewayResponse.getStatusCode());
-        assertEquals(this.productId,productResponse.getId());
-        assertEquals(PRODUCT_NAME, productResponse.getName());
-        assertEquals(PRODUCT_CATEGORY, productResponse.getCategory());
-        assertEquals(PRODUCT_DESCRIPTION, productResponse.getDescription());
-        assertEquals(PRODUCT_NEW_PRICE, productResponse.getPrice());
+        assertEquals("Xbox Series S", productResponse.getName());
+        assertEquals("Console", productResponse.getCategory());
+        assertEquals("Microsoft next generation console", productResponse.getDescription());
+        assertEquals(2700.00, productResponse.getPrice());
     }
 
     @Test
     @Order(4)
     void mustListProductsWithSucess() throws JsonProcessingException {
-
         AwsProxyRequest request = new AwsProxyRequestBuilder("/products", HttpMethod.GET.toString())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .build();
 
         final var apiGatewayResponse = handleApiGatewayRequest(request);
-        final var productResponseList = objectMapper.readValue(apiGatewayResponse.getBody(), new TypeReference<List<ProductResponse>>(){});
-
-        final var productResponse = productResponseList.stream().findFirst().get();
+        final var productResponseList = objectMapper.readValue(apiGatewayResponse.getBody(), new TypeReference<List<ProductResponse>>() {
+        });
 
         assertEquals(HttpStatus.OK.getCode(), apiGatewayResponse.getStatusCode());
-        assertEquals(this.productId,productResponse.getId());
-        assertEquals(PRODUCT_NAME, productResponse.getName());
-        assertEquals(PRODUCT_CATEGORY, productResponse.getCategory());
-        assertEquals(PRODUCT_DESCRIPTION, productResponse.getDescription());
-        assertEquals(PRODUCT_NEW_PRICE, productResponse.getPrice());
+
+        assertEquals("Playstation 5", productResponseList.get(0).getName());
+        assertEquals("Console", productResponseList.get(0).getCategory());
+        assertEquals("Sony next generation console", productResponseList.get(0).getDescription());
+        assertEquals(4500.00, productResponseList.get(0).getPrice());
+
+        assertEquals("Iphone 12", productResponseList.get(1).getName());
+        assertEquals("Smarthphone", productResponseList.get(1).getCategory());
+        assertEquals("Apple Smarthphone", productResponseList.get(1).getDescription());
+        assertEquals(7500.00, productResponseList.get(1).getPrice());
+
+        assertEquals("Xbox Series S", productResponseList.get(2).getName());
+        assertEquals("Console", productResponseList.get(2).getCategory());
+        assertEquals("Microsoft next generation console", productResponseList.get(2).getDescription());
+        assertEquals(2700.00, productResponseList.get(2).getPrice());
     }
 
     @Test
     @Order(5)
-    void mustDeleteProductWithSucess() {
+    void mustDeleteProductWithSucess() throws JsonProcessingException {
 
-        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(this.productId), HttpMethod.DELETE.toString())
+        final var product = createProduct("Xbox Series X", 5800.00, "Microsoft next generation console", "Console");
+
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/products/".concat(product.getId()), HttpMethod.DELETE.toString())
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
                 .build();
 
@@ -133,4 +138,24 @@ public class ProductControllerIntegrationTestTest extends IntegrationControllerT
 
         assertEquals(HttpStatus.NO_CONTENT.getCode(), apiGatewayResponse.getStatusCode());
     }
+
+    private ProductResponse createProduct(String name, Double price, String description, String category) throws JsonProcessingException {
+
+        final ProductRequest productRequest = ProductRequest.builder()
+                .name(name)
+                .price(price)
+                .description(description)
+                .category(category)
+                .build();
+
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/products", HttpMethod.POST.toString())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .body(createJsonBoby(productRequest))
+                .build();
+
+        final var response = handleApiGatewayRequest(request);
+
+        return objectMapper.readValue(response.getBody(), ProductResponse.class);
+    }
+
 }
